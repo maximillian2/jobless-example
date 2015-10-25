e3346c529ca7eab55f4b8f107a47230b9e30a125
maximillian2 <maxi.ua.1996@gmail.com>
2015-10-25 20:10:40 +0200

    First version

--- example-cv.rb
+++ example-cv.rb
@@ -1,57 +1,94 @@
 require 'jobless'
 
 Jobless.cv do
-  name "Muffin The Cat"
-  email "muffin@catpirates.com"
-  location "Zagreb, Croatia"
-  address "Unska 11"
-  homepage "http://dabrorius.github.io"
-
-  employment do
-    entry do
-      title "Full-stack Lazy Slob"
-      company "Google"
-      homepage "http://google.com/"
-      technologies "Ruby, Ruby on Rails, Javascript"
-      bulletin "Layed on important papers."
-      bulletin "Scratched all the chairs."
-      bulletin "Thrown all of the sand out of the litterbox."
-      start_date "June 2015"
-      end_date "Current"
-    end
-    entry do
-      title "Senior Trouble Maker "
-      company "Facebook"
-      homepage "https://www.facebook.com/"
-      technologies "Ruby, Ruby on Rails, Elasticsearch"
-      bulletin "Ate Mark Zuckerberg's sandwich."
-      start_date "April 2015"
-      end_date "June 2015"
-    end
-  end
+  name "Maksym Tymoshyk"
+  email "maksym.tymoshyk@gmail.com"
+  location "Kiev, Ukraine"
+  homepage "https://github.com/maximillian2"
 
   education do
     entry do
-      title "M.S. Food Science"
-      company "The University of Zagreb"
+      title "SE Junior Specialist"
+      company "Kiev College of Computer Technologies and Economics"
       start_date "2011"
-      end_date "2013"
+      end_date "2015"
+    end
+    
+    entry do
+      title "SE Bachelor"
+      company "National Aviation University (2nd course)"
+      start_date "2015"
+      end_date "2018"
     end
   end
 
+  group "Achievements" do
+    entry do 
+      title "English City Olympiads"
+      description "Top places"
+      start_date "2012"
+      end_date "2014"
+    end
+    
+    entry do 
+      title "Programming City Olympiad (C++)"
+      description "2nd place"
+      start_date "2014"
+    end
+    
+    entry do
+      title "Course Work"
+      description "Economic and financial series crossplatform modelling tool using Hidden Markov Models."
+      technologies "C++, STL, Qt, gcc, make, git"
+      start_date "2014"
+      homepage "https://github.com/maximillian2/time-series"
+    end
+    
+    entry do 
+      title "Course Work"
+      technologies "C++, Qt, gcc, make, git"
+      description "Crossplatform HTML editor with build-in previewer and syntax highlight."
+      start_date "2014"
+      homepage "https://github.com/maximillian2/html-editor-opensource"
+    end
+    
+    entry do
+      title "Diploma Work"
+      description "Web-service to systemize watched video content."
+      technologies "Ruby, Rails, JS, PostreSQL, Bootstrap, git"
+      start_date "2015"
+      homepage "http://videodepot.herokuapp.com/"
+    end
+  end
+  
+  group "Skills" do
+    entry do 
+      title "English: upper-intermediate"
+    end
+    
+    entry do 
+      bulletin "Ruby: Rails, RSpec"
+      bulletin "Linux: bash, zsh, gcc, make"
+      bulletin "Databases: SQLite, MySQL, PostreSQL"
+      bulletin "C++ : STL, Qt"
+      bulletin "HTML, CSS, JS, Bootstrap"
+      bulletin "CVS (git)"
+      bulletin "Python"
+    end
+  end
+  
   open_source do
     entry do
-      title "markov-noodles"
-      homepage "https://github.com/dabrorius/markov-noodles"
-      description "Markov noodles is a minimalistic library for generating "\
-        "text using Markov chains."
+      title "AdBlock ukrainian translation"
+      homepage "https://getadblock.com/"
+      description "Translations made with CrowdIn service."
     end
   end
 
   other_experience do
     entry do
-      title "Tree Climbing"
-      homepage "https://en.wikipedia.org/wiki/Tree"
+      title "CrowdIn activity"
+      homepage "sample_text"
     end
   end
 end
